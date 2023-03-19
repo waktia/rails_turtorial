886079ee60e292ff1cae1209f49a8a346f3b1b17
Ubuntu <ubuntu@ip-172-31-32-136.ap-northeast-1.compute.internal>
2023-03-19 07:31:58 +0000

    use ssl and the puma in production

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,9 +10,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password:              "foo",
                                          password_confirmation: "bar" } }
     end
-    assert_template 'users/new'
-    assert_select 'div#<CSS id for error explanation>'
-    assert_select 'div.<CSS class for field with error>'
+    
   end
   
   test "valid signup information" do
@@ -25,5 +23,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
+    
   end
 end
